c72ae770c626c53234d4a1885dc7704d11864b01
Kshitiz Rana <ranakshitizgm@gmail.com>
2025-07-14 13:38:18 +0530

    feat: add express server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db";
+import app from "./app";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8080;
+
+connectDB()
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((err) => {
+    console.log("MongoDB connection Error", err);
+    process.exit(1);
+  });
