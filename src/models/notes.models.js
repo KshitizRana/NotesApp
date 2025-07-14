3d7dea308ddaf0bf8cee71ce3ae0c2cf17bec109
Kshitiz Rana <ranakshitizgm@gmail.com>
2025-07-14 15:48:27 +0530

    feat: add notes database model

---
+++ src/models/notes.models.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const notesSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Notes = mongoose.model("Notes", notesSchema);
